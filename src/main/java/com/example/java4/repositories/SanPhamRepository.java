91b7cc6e69fe985b7c368587958930c8bcbd975b
TaiSimle with Zamato <tuantain2003@gmail.com>
2024-05-08 17:13:27 +0700

    Demo Cart

--- src/main/java/com/example/java4/repositories/SanPhamRepository.java
+++ src/main/java/com/example/java4/repositories/SanPhamRepository.java
@@ -4,9 +4,12 @@ import com.example.java4.entities.MauSac;
 import com.example.java4.entities.SanPham;
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 
 @Repository
 public interface SanPhamRepository
@@ -15,4 +18,9 @@ public interface SanPhamRepository
     public static final int ACTIVE  = 1;
     public static final int INACTIVE =0;
     public Page<SanPham> findByTrangThai(int trangThai, Pageable pageable);
+
+
+    // Query để lấy ra 3 sản phẩm duy nhất (distinct) từ cơ sở dữ liệu
+    @Query("SELECT DISTINCT sp FROM SanPham sp ")
+    List<SanPham> getDistinctTopBy3(Pageable pageable);
 };
